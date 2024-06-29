228591eec1da7f7bf812e0b48a8d04b9b6fec7a0
SedationH <sedationh@gmail.com>
2024-06-29 10:49:45 +0800

    部署合约

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
     sepolia: {
       url:
         "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
-    },
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
   },
 };
